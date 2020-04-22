157a5c366ebdbca115caf4b8680ab3bd664c88fa
sarawattine <62794622+sarawattine@users.noreply.github.com>
2020-04-23 03:08:54 +0800

    week7

---
+++ a108060096_week7_1.py
@@ -0,0 +1,12 @@
+＃從維基百科找出戲劇資訊
+＃＃https://zh.wikipedia.org/wiki/梨泰院Class
+＃＃相關資訊：戲劇級數、發表日期、評分
+
+
+
+drama_title = " sweet night" #戲劇名稱
+release_year = "2020" #發表日期
+drama_time = "60 min" #每集長度
+drama_rating = "8.8" ＃評分
+
+
